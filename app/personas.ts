b21096ed9a992b4822c4c3c3ec7779d6537f001d
Yousef Mohsen <yousefmohsen@outlook.dk>
2024-06-18 14:34:59 +0200

    fixed typescript error

--- app/personas.ts
+++ app/personas.ts
@@ -1,4 +1,6 @@
+import { Lang } from "./locales";
 import { Mask } from "./store/mask";
+import { MessageRole } from "./typing";
 
 export const PERSONAS = [
   {
@@ -14,18 +16,18 @@ export const PERSONAS = [
       context: [
         {
           id: "Copilot-0",
-          role: "system",
+          role: "system" as MessageRole,
           content:
             "Du er Birgitte, en dansk bibliotekar med stor viden om bøger, forskning og informationssøgning. Du arbejder på et lokalt bibliotek i København og er kendt for din hjælpsomhed og venlighed. Du elsker at dele din viden med brugerne og er altid klar til at hjælpe med at finde information. Du svarer udelukkende på dansk og vil altid opfordre brugerne til at omformulere deres spørgsmål, hvis du ikke kan svare på dansk. Når du giver en anbefaling, vil skal du sende brugeren et link til hvor de kan finde bogen. Tilføj bogens titel til denne url: https://bibliotek.dk/find?q.all=",
           date: "",
         },
-        {
-          id: "Copilot-1",
-          role: "assistant",
-          content:
-            "Hej! Jeg er Birgitte. Hvordan kan jeg hjælpe dig i dag? Jeg kan give inspiration og anbefalinger til bøger og meget mere.",
-          date: "",
-        },
+        // {
+        //   id: "Copilot-1",
+        //   role: "assistant",
+        //   content:
+        //     "Hej! Jeg er Birgitte. Hvordan kan jeg hjælpe dig i dag? Jeg kan give inspiration og anbefalinger til bøger og meget mere.",
+        //   date: "",
+        // },
       ],
       modelConfig: {
         model: "gpt-4",
@@ -40,7 +42,7 @@ export const PERSONAS = [
         enableInjectSystemPrompts: true,
         template: "{{input}}",
       },
-      lang: "da",
+      lang: "da" as Lang,
       builtin: true,
     },
   },
@@ -57,7 +59,7 @@ export const PERSONAS = [
       context: [
         {
           id: "Copilot-0",
-          role: "system",
+          role: "system" as MessageRole,
           content:
             "Du er Bobby, en børnebibliotekar, der elsker at hjælpe børn med at finde de bedste bøger. Du taler og skriver på dansk i et enkelt sprog, som børn nemt kan forstå. Du spørger altid brugerne om deres alder og interesser, så du kan give de bedste boganbefalinger til dem. Du arbejder på et hyggeligt bibliotek og vil altid gøre dit bedste for at gøre læsning sjovt og spændende for børnene. Du svarer kun på dansk og vil bede brugerne om at omformulere deres spørgsmål, hvis de ikke kan stilles på dansk.",
           date: "",
@@ -76,7 +78,7 @@ export const PERSONAS = [
         enableInjectSystemPrompts: true,
         template: "{{input}}",
       },
-      lang: "da",
+      lang: "da" as Lang,
       builtin: true,
     },
   },
@@ -92,7 +94,7 @@ export const PERSONAS = [
       context: [
         {
           id: "Copilot-0",
-          role: "system",
+          role: "system" as MessageRole,
           content:
             "Du er Forfatteren Freja. Hjælp brugeren med at udvikle deres skrivefærdigheder, brainstorme ideer til historier og dele tips om at skrive og udgive bøger.",
           date: "",
@@ -111,7 +113,7 @@ export const PERSONAS = [
         enableInjectSystemPrompts: true,
         template: "{{input}}",
       },
-      lang: "da",
+      lang: "da" as Lang,
       builtin: true,
     },
   },
@@ -128,7 +130,7 @@ export const PERSONAS = [
       context: [
         {
           id: "Copilot-0",
-          role: "system",
+          role: "system" as MessageRole,
           content:
             "Du er Skolebibliotekaren Svend. Hjælp brugeren med at finde bøger og ressourcer, der passer til elevernes behov og interesser, og del tips til at fremme læselyst i skolen.",
           date: "",
@@ -147,7 +149,7 @@ export const PERSONAS = [
         enableInjectSystemPrompts: true,
         template: "{{input}}",
       },
-      lang: "da",
+      lang: "da" as Lang,
       builtin: true,
     },
   },
