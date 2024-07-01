3d287b5d55bbb359d66617ff928dd7887cc0b9c2
Yousef Mohsen <yousefmohsen@outlook.dk>
2024-07-01 15:44:07 +0200

    accordion animaiton

--- app/components/chatbib-info.tsx
+++ app/components/chatbib-info.tsx
@@ -1,11 +1,10 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { IconButton } from "./button";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Path } from "../constant";
 import Locale from "../locales";
 import styles from "./chatbib-info.module.scss";
 import content from "./chatbib-info-texts.json";
-import DownIcon from "../icons/down.svg";
 
 const introText =
   "For at understøtte udviklingen af AI-produkter i bibliotekerne har KOMBIT og DBC lanceret ChatBib. Projektet er designet til at fremme anvendelsen af AI-teknologier i biblioteker gennem praktiske eksperimenter og vidensdeling. ChatBib udnytter åbne AI-modeller i et sikkert og transparent miljø, som ikke indgår i tech-giganternes forretningsmodeller, hvilket beskytter brugernes data. Formålet er at udforske potentialer og begrænsninger ved AI for at kunne udvikle fremtidige AI-løsninger i bibliotekssektoren. ChatBib er åbent for alle og kan bruges til både professionel udvikling og inspiration i andre offentlige sektorer. Projektet sigter også på at sikre høje standarder for databeskyttelse og privatliv.";
