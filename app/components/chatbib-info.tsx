b78ce5e3c75eeca6ef30052d3ed2667d6dd35d65
Yousef Mohsen <yousefmohsen@outlook.dk>
2024-07-01 16:12:37 +0200

    down arrow + logo + animaitons

--- app/components/chatbib-info.tsx
+++ app/components/chatbib-info.tsx
@@ -5,6 +5,7 @@ import { Path } from "../constant";
 import Locale from "../locales";
 import styles from "./chatbib-info.module.scss";
 import content from "./chatbib-info-texts.json";
+import DownIcon from "../icons/down.svg";
 
 const introText =
   "For at understøtte udviklingen af AI-produkter i bibliotekerne har KOMBIT og DBC lanceret ChatBib. Projektet er designet til at fremme anvendelsen af AI-teknologier i biblioteker gennem praktiske eksperimenter og vidensdeling. ChatBib udnytter åbne AI-modeller i et sikkert og transparent miljø, som ikke indgår i tech-giganternes forretningsmodeller, hvilket beskytter brugernes data. Formålet er at udforske potentialer og begrænsninger ved AI for at kunne udvikle fremtidige AI-løsninger i bibliotekssektoren. ChatBib er åbent for alle og kan bruges til både professionel udvikling og inspiration i andre offentlige sektorer. Projektet sigter også på at sikre høje standarder for databeskyttelse og privatliv.";
@@ -20,10 +21,11 @@ export function ChatbibIntro() {
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <div>
-          <h1>Velkommen til ChatBib</h1>
-          <p>{introText}</p>
+        <div className={styles.logoContainer}>
+          <img src="/dbclogo.png" style={{ width: "100px" }} />
         </div>
+        <h1>Velkommen til ChatBIB</h1>
+        <p>{introText}</p>
         <div className={styles.buttonContainer}>
           <IconButton
             className={styles["new-chat-button"]}
@@ -44,7 +46,11 @@ export function ChatbibIntro() {
                 id={item.id}
                 onClick={() => handleToggle(item.id)}
               >
-                <h3 className={styles.accordionTitle}>{item.title}</h3>
+                <div className={styles.accordionTitle}>
+                  <h3>{item.title}</h3>
+                  <DownIcon className={styles.arrowIcon} />
+                </div>
+
                 {isOpen && <p>{item.text}</p>}
               </div>
             );
