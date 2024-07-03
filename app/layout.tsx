1b10f4f25f0cb7e57723fad0f32fa2e9f7934e00
Yousef Mohsen <yousefmohsen@outlook.dk>
2024-07-03 12:19:37 +0200

    added matomo

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,6 +42,22 @@ export default function RootLayout({
         />
         <link rel="manifest" href="/site.webmanifest"></link>
         <script src="/serviceWorkerRegister.js" defer></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(["disableCookies"]);
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="https://stats.dbc.dk/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '42']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();`,
+          }}
+        />
       </head>
       <body>
         {children}
