a7787b9db99cc7ddbebbd34c08eb89f2df329deb
Kasper Fuglsang <kfu@dbc.dk>
2024-08-13 13:21:26 +0200

    fixing runtime variables

--- app/[...pages]/page.tsx
+++ app/[...pages]/page.tsx
@@ -1,6 +1,9 @@
+import { unstable_noStore as noStore } from "next/cache";
+
 import { Home } from "../components/home";
 
 export default async function App() {
+  noStore();
   return (
     <>
       <Home />
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
+import { unstable_noStore as noStore } from "next/cache";
 import { Home } from "./components/home";
 
 export default async function App() {
+  noStore();
   return (
     <>
       <Home />
