2fc56d9ae439c8da363999e9b057944c0c81f9cc
Yousef Mohsen <yousefmohsen@outlook.dk>
2024-09-04 14:43:02 +0200

    fix build

--- app/dbc/components/hooks/useGlobalState.js
+++ app/dbc/components/hooks/useGlobalState.js
@@ -1,3 +1,4 @@
+"use client";
 import useSWR, { mutate } from "swr";
 let globalState = {};
 
--- app/locales/index.ts
+++ app/locales/index.ts
@@ -1,4 +1,3 @@
-import en from "./en";
 import da, { LocaleType } from "./da";
 
 import { merge } from "../utils/merge";
