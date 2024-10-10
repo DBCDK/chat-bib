603dcd8759a4a7113a7215a25fe832144fc2dc1b
Yousef Mohsen <yousefmohsen@outlook.dk>
2024-10-10 09:02:11 +0200

    block cookies

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,6 +36,21 @@ export default function RootLayout({
   return (
     <html lang="da">
       <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `   var _paq = window._paq = window._paq || [];
+        //  _paq.push(['trackPageView']);
+            _paq.push(["requireCookieConsent"]);     // <--- Add this line to the script
+          _paq.push(['enableLinkTracking']);
+          (function() {
+            var u="https://stats.dbc.dk/";
+            _paq.push(['setTrackerUrl', u+'matomo.php']);
+            _paq.push(['setSiteId', '42']);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+            })();`,
+          }}
+        />
         <Script
           id="Cookiebot"
           src="https://consent.cookiebot.com/uc.js"
@@ -50,22 +65,38 @@ export default function RootLayout({
         />
         <link rel="manifest" href="/site.webmanifest"></link>
         <script src="/serviceWorkerRegister.js" defer></script>
+
         <script
           dangerouslySetInnerHTML={{
-            __html: `  var _paq = window._paq = window._paq || [];
-  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-  _paq.push(["disableCookies"]);
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="https://stats.dbc.dk/";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '42']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-  })();`,
+            __html: `
+// Cookiebot consent and Matomo connector
+  var waitForTrackerCount = 0;
+  function matomoWaitForTracker() {
+    if (typeof _paq === "undefined" || typeof Cookiebot === "undefined") {
+      if (waitForTrackerCount < 40) {
+        setTimeout(matomoWaitForTracker, 250);
+        waitForTrackerCount++;
+        return;
+      }
+    } else {
+      window.addEventListener("CookiebotOnAccept", function (e) {
+        consentSet();
+      });
+      window.addEventListener("CookiebotOnDecline", function (e) {
+        consentSet();
+      });
+    }
+  }
+  function consentSet() {
+    if (Cookiebot.consent.statistics) {
+      _paq.push(["setCookieConsentGiven"]);
+    } else {
+      _paq.push(["forgetCookieConsentGiven"]);
+    }
+  }
+  document.addEventListener("DOMContentLoaded", matomoWaitForTracker());`,
           }}
-        />
+        ></script>
       </head>
       <body>
         {children}
